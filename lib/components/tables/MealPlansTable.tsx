575f9c5a1a7f354c131f27d6343489d97a1b33eb
Hinokamika <manh.ndm2506@gmail.com>
2025-11-18 10:21:33 +0700

    fix: enable explicit any type for MealPlansTable component

--- lib/components/tables/MealPlansTable.tsx
+++ lib/components/tables/MealPlansTable.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
 import { Table, Space, Popconfirm, Tooltip, Button, Input, DatePicker, notification } from "antd";
 import { SearchOutlined } from "@ant-design/icons";
